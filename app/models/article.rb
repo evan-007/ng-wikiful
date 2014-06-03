e9177bdf8a88eed3695207d1a957e25ef9235667
Evan <eulloyd@gmail.com>
2014-06-03 05:58:27 +0000

    Add inverse relationships to pass validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,4 @@ class Article < ActiveRecord::Base
 	validates :body, presence: true
   has_many :article_categories, inverse_of: :article
 	has_many :categories, through: :article_categories
-
-	accepts_nested_attributes_for :categories
 end
--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -2,5 +2,4 @@ class ArticleCategory < ActiveRecord::Base
   belongs_to :article, inverse_of: :article_categories
   belongs_to :category, inverse_of: :article_categories
   validates :category, :article, presence: true
-  accepts_nested_attributes_for :category
 end
