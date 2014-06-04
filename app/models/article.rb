3b62ff203e5820ee934382b8997c093103d88d44
Evan Lloyd <eulloyd@gmail.com>
2014-06-04 15:17:24 +0900

    Add inverse of to article/category relationship

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true
   has_many :article_categories, inverse_of: :article
-	has_many :categories, through: :article_categories
+	has_many :categories, through: :article_categories, inverse_of: :articles
 end
